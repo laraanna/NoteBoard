2dcc53febfaf20bca4fd13c89af18662d83f2f1b
laraanna <lara@laraannawagner.com>
2018-02-12 11:24:10 +0100

    Set up Notes Container

---
+++ src/components/NotesContainer.js
@@ -0,0 +1,13 @@
+import React, {PureComponent} from 'react'
+
+class NotesContainer extends PureComponent {
+  render(){
+    return(
+      <div className="NotesContainer">
+        Notes
+      </div>
+    )
+  }
+}
+
+export default NotesContainer;
