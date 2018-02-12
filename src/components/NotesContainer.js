abae9e102e3fe1c4935521c15829733aaff8429e
laraanna <lara@laraannawagner.com>
2018-02-12 11:42:07 +0100

    Initialize State in Notes Container

--- src/components/NotesContainer.js
+++ src/components/NotesContainer.js
@@ -1,6 +1,12 @@
 import React, {PureComponent} from 'react'
 
 class NotesContainer extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      notes: []
+    }
+  }
   render(){
     return(
       <div className="NotesContainer">
