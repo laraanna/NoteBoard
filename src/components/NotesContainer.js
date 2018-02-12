95a5217e7b39390fb0aa6629d7adae7c55f5a5f1
laraanna <lara@laraannawagner.com>
2018-02-12 11:59:34 +0100

    Fetch Notes Data from Api

--- src/components/NotesContainer.js
+++ src/components/NotesContainer.js
@@ -1,4 +1,5 @@
 import React, {PureComponent} from 'react'
+import axios from 'axios'
 
 class NotesContainer extends PureComponent {
   constructor(props) {
@@ -7,10 +8,29 @@ class NotesContainer extends PureComponent {
       notes: []
     }
   }
+
+  componentDidMount(){
+    axios.get('http://localhost:3001/api/v1/notes.json')
+    .then(res => {
+      console.log(res)
+      this.setState({notes: res.data})
+    })
+    .catch(err => console.log(err))
+  }
+
+  renderOneNote = (note) => {
+    return(
+      <div className="Note" key={note.id}>
+        <h1>{note.title}</h1>
+        <p> {note.body}</p>
+      </div>
+    )
+  }
+
   render(){
     return(
       <div className="NotesContainer">
-        Notes
+        {this.state.notes.map(this.renderOneNote)}
       </div>
     )
   }
