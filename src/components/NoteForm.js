10f6b26e9975e98717507e734eaea4c1b6c71cb5
laraanna <lara@laraannawagner.com>
2018-02-16 11:47:34 +0100

    Create Handle Input function

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -6,16 +6,22 @@ class NoteForm extends PureComponent {
   constructor(props) {
     super(props)
     this.state = {
+      title: this.props.note.title,
+      body: this.props.note.body
     }
+  }
+
+  handleInput = (e) => {
+    this.setState({[e.target.name]: e.target.value})
   }
   render(){
     return(
       <div className="Note">
         <form>
           <input className='input' type="text"
-            name="Note" placeholder='Enter a Title' />
+            name="Note" placeholder='Enter a Title' value={this.state.title} onChange={this.handleInput} />
           <textarea className='input' name="body"
-            placeholder='Describe your note'></textarea>
+            placeholder='Describe your note' value={this.state.body} onChange={this.handleInput}></textarea>
         </form>
       </div>
     )
